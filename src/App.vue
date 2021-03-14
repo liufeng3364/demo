0fafc6f579f071ae5624fce0326edf7ca0bfaf65
liufeng3364 <1220393274@qq.com>
2021-03-14 15:20:08 +0800

    fix: 添加element依赖

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <el-row>
+      <el-col :span="24">
+        <el-container>
+          login
+        </el-container>
+      </el-col>
+    </el-row>
   </div>
-  <router-view/>
 </template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 1200px;
+  margin: 0 auto;
+
+}
+.el-container{
+  width: 700px;
+  height: 500px;
+  margin: 100px auto;
+  background: #409EFF;
+
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
